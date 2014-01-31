69169962a7b68e50be0262fb3a4bfafeee3c18ef
cttn <cj.cttn@gmail.com>
2014-01-30 23:01:44 -0300

    Added minor details

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name='Stocks',
     version='0.1dev',
+    description='Python 3 wrapper for Yahoo! Finance API',
+    author='Cttn',
+    author_email='cj.cttn@gmail.com',
     packages=['stockex',],
     license='',
     long_description=open('README.md').read(),
