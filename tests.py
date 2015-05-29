836634125ebf2b896614790183f5231a3b3e95b3
josuebrunel <josuebrunel@gmail.com>
2015-05-29 15:09:48 +0200

    get_historical test added

--- tests.py
+++ tests.py
@@ -2,12 +2,17 @@
 """
 from unitttest import TestCase
 
+from stockex import stockwrapper as sw
+
 class TestStockex(TestCase):
 
     def setUp(self):
-        pass
+        data = sw.YahooData()
 
     def tearDown(self):
         pass
 
+    def test_get_historical(self,):
 
+        result = get_historical('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
+        print(result)
