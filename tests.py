e0002413b7048fe06fd2215dc11ab7f191912440
josuebrunel <josuebrunel@gmail.com>
2015-05-29 15:44:02 +0200

    test for retrocompatibility added

--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 """Module to run tests
 """
+import logging
+
 from unittest import TestCase
 
 from stockex import stockwrapper as sw
+
+logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(name)s.%(module)s.%(funcName)s] %(message)s \n")
+logging.getLevelName('TestStockex')
 
 class TestStockex(TestCase):
 
@@ -13,6 +18,10 @@ class TestStockex(TestCase):
         pass
 
     def test_get_historical(self,):
-
+        logging.debug("with full list of params")
         result = self.data.get_historical('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
-        print(result)
+        logging.debug(result)
+
+        logging.debug("only with symbol")
+        result = self.data.get_historical('YHOO')
+        logging.debug(result)
