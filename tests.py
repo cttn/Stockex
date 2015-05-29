749134d29413998cd726dda469933464865e8e6c
josuebrunel <josuebrunel@gmail.com>
2015-05-29 15:36:12 +0200

    test_get_historical added

--- tests.py
+++ tests.py
@@ -1,18 +1,18 @@
 """Module to run tests
 """
-from unitttest import TestCase
+from unittest import TestCase
 
 from stockex import stockwrapper as sw
 
 class TestStockex(TestCase):
 
     def setUp(self):
-        data = sw.YahooData()
+        self.data = sw.YahooData()
 
     def tearDown(self):
         pass
 
     def test_get_historical(self,):
 
-        result = get_historical('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
+        result = self.data.get_historical('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
         print(result)
