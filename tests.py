07420c0832414fe0e3f1f39d6b6cec20827a6651
cttn <cj.cttn@gmail.com>
2015-06-05 18:47:33 -0300

    flake8 fixes

--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
 """Module to run tests
 """
 import logging
-
 from unittest import TestCase
-
 from stockex import stockwrapper as sw
 
-logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(name)s.%(module)s.%(funcName)s] %(message)s \n")
+logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s %(levelname)s]" +
+                    "[%(name)s.%(module)s.%(funcName)s] %(message)s \n")
 logging.getLevelName('TestStockex')
+
 
 class TestStockex(TestCase):
 
@@ -19,9 +19,13 @@ class TestStockex(TestCase):
 
     def test_get_historical(self,):
         logging.debug("with full list of params")
-        result = self.data.get_historical('YHOO',['Open','Close','High','Low'],startDate='2014-09-11',endDate='2015-02-10',limit=5)
+        result = self.data.get_historical(
+                'YHOO',
+                ['Open', 'Close', 'High', 'Low'],
+                startDate='2014-09-11',
+                endDate='2015-02-10',
+                limit=5)
         logging.debug(result)
-
         logging.debug("only with symbol")
         result = self.data.get_historical('YHOO')
         logging.debug(result)
